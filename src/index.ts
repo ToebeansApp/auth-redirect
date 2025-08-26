a74f3054ddbc8325279dbd33099cfb950a235be5
Jordon de Hoog <jordon.dehoog@gmail.com>
2025-08-25 20:04:52 -0400

    update proxy

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 export default {
   async fetch(request: Request): Promise<Response> {
     const url = new URL(request.url);
-    if (!url.pathname.startsWith("/__/auth/")) {
-      return new Response("Not found", { status: 404 });
-    }
-
     const targetUrl = `https://toebeans-421217.firebaseapp.com${url.pathname}${url.search}`;
     const init: RequestInit = {
       method: request.method,
